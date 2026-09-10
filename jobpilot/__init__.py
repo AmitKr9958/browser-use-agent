430646bf6fed80f0393f4f79b468e2afb13b4e88
AmitKr9958 <58068340+AmitKr9958@users.noreply.github.com>
2026-09-10 19:59:30 +0530

    Expose JobPilot public API

--- jobpilot/__init__.py
+++ jobpilot/__init__.py
@@ -1,6 +1,18 @@
 """JobPilot application layer built on Browser Use."""
 
+from .autofill import build_field_values, classify_field, map_form_fields
 from .models import Job, JobApplicationDraft, ResumeProfile
 from .pipeline import JobPilot
+from .resume_io import read_resume, write_ats_docx
 
-__all__ = ["Job", "JobApplicationDraft", "JobPilot", "ResumeProfile"]
+__all__ = [
+    "Job",
+    "JobApplicationDraft",
+    "JobPilot",
+    "ResumeProfile",
+    "build_field_values",
+    "classify_field",
+    "map_form_fields",
+    "read_resume",
+    "write_ats_docx",
+]
