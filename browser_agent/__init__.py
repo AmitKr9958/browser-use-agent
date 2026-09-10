bf12f29be8be4c32f8779ae4c09b4bc33bdfe5f7
AmitKr9958 <58068340+AmitKr9958@users.noreply.github.com>
2026-09-10 20:25:28 +0530

    Expose LLM provider factory in public API

--- browser_agent/__init__.py
+++ browser_agent/__init__.py
@@ -2,6 +2,7 @@
 
 from .actions.basic import click_selector, open_url, screenshot
 from .agents import run_on_tab
+from .llm import create_llm
 from .models.india import DEFAULT_INDIA_RUNTIME, IndiaRuntimeConfig
 from .models.policy import DEFAULT_SENSITIVE_POLICY, SensitiveInteractionPolicy
 from .tabs.errors import AmbiguousTabError, TabNotFoundError, TabVerificationError
@@ -20,6 +21,7 @@ __all__ = [
     "TabSelector",
     "TabVerificationError",
     "click_selector",
+    "create_llm",
     "open_url",
     "run_on_tab",
     "screenshot",
