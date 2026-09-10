0f7cecbc890394994fbbf2e247baaf83040b43dc
AmitKr9958 <58068340+AmitKr9958@users.noreply.github.com>
2026-09-10 22:07:05 +0530

    Export JobPilot review and upload APIs

--- jobpilot/__init__.py
+++ jobpilot/__init__.py
@@ -2,15 +2,16 @@
 
 from .ats import ATS, detect_ats
 from .autofill import build_field_values, classify_field, map_form_fields
+from .browser_upload import upload_file_by_dom_index
 from .career_pages import CareerPage, CareerPageRegistry
 from .field_policy import is_safe_autofill_label
-from .models import Job, JobApplicationDraft, ResumeProfile
+from .models import ApplicationReview, Job, JobApplicationDraft, ResumeProfile
 from .pipeline import JobPilot
 from .resume_io import read_resume, write_ats_docx
 
 __all__ = [
-    "ATS", "CareerPage", "CareerPageRegistry", "Job", "JobApplicationDraft",
-    "JobPilot", "ResumeProfile", "build_field_values", "classify_field",
-    "detect_ats", "is_safe_autofill_label", "map_form_fields", "read_resume",
-    "write_ats_docx",
+    "ATS", "ApplicationReview", "CareerPage", "CareerPageRegistry", "Job",
+    "JobApplicationDraft", "JobPilot", "ResumeProfile", "build_field_values",
+    "classify_field", "detect_ats", "is_safe_autofill_label", "map_form_fields",
+    "read_resume", "upload_file_by_dom_index", "write_ats_docx",
 ]
