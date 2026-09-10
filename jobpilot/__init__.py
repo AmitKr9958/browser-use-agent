592d4f34bc62d4c3bfafd1be7e436d59fdc51eb9
AmitKr9958 <58068340+AmitKr9958@users.noreply.github.com>
2026-09-10 21:43:31 +0530

    Expose JobPilot ATS and career-page APIs

--- jobpilot/__init__.py
+++ jobpilot/__init__.py
@@ -1,18 +1,16 @@
 """JobPilot application layer built on Browser Use."""
 
+from .ats import ATS, detect_ats
 from .autofill import build_field_values, classify_field, map_form_fields
+from .career_pages import CareerPage, CareerPageRegistry
+from .field_policy import is_safe_autofill_label
 from .models import Job, JobApplicationDraft, ResumeProfile
 from .pipeline import JobPilot
 from .resume_io import read_resume, write_ats_docx
 
 __all__ = [
-    "Job",
-    "JobApplicationDraft",
-    "JobPilot",
-    "ResumeProfile",
-    "build_field_values",
-    "classify_field",
-    "map_form_fields",
-    "read_resume",
+    "ATS", "CareerPage", "CareerPageRegistry", "Job", "JobApplicationDraft",
+    "JobPilot", "ResumeProfile", "build_field_values", "classify_field",
+    "detect_ats", "is_safe_autofill_label", "map_form_fields", "read_resume",
     "write_ats_docx",
 ]
