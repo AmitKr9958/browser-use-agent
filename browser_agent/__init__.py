92b685021ae062b26a69b3c28061ddd8bd3fe805
AmitKr9958 <58068340+AmitKr9958@users.noreply.github.com>
2026-09-10 19:32:15 +0530

    Expose interaction policy in public API

--- browser_agent/__init__.py
+++ browser_agent/__init__.py
@@ -2,6 +2,7 @@
 
 from .agents import run_on_tab
 from .models.india import DEFAULT_INDIA_RUNTIME, IndiaRuntimeConfig
+from .models.policy import DEFAULT_SENSITIVE_POLICY, SensitiveInteractionPolicy
 from .tabs.errors import AmbiguousTabError, TabNotFoundError, TabVerificationError
 from .tabs.manager import TabManager
 from .tabs.models import TabRecord, TabSelector
@@ -9,7 +10,9 @@ from .tabs.models import TabRecord, TabSelector
 __all__ = [
     "AmbiguousTabError",
     "DEFAULT_INDIA_RUNTIME",
+    "DEFAULT_SENSITIVE_POLICY",
     "IndiaRuntimeConfig",
+    "SensitiveInteractionPolicy",
     "TabManager",
     "TabNotFoundError",
     "TabRecord",
