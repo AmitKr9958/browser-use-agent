aa90f65a3ec5f95a8270e5fc8f0111e2c4871a06
AmitKr9958 <58068340+AmitKr9958@users.noreply.github.com>
2026-09-10 19:31:53 +0530

    Expose production interaction policy

--- browser_agent/models/__init__.py
+++ browser_agent/models/__init__.py
@@ -1,5 +1,11 @@
 """Application runtime models."""
 
 from .india import DEFAULT_INDIA_RUNTIME, IndiaRuntimeConfig
+from .policy import DEFAULT_SENSITIVE_POLICY, SensitiveInteractionPolicy
 
-__all__ = ["DEFAULT_INDIA_RUNTIME", "IndiaRuntimeConfig"]
+__all__ = [
+    "DEFAULT_INDIA_RUNTIME",
+    "DEFAULT_SENSITIVE_POLICY",
+    "IndiaRuntimeConfig",
+    "SensitiveInteractionPolicy",
+]
